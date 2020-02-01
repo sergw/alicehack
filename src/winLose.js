de2b2847044b2ed5cd7b8ccf670c3de3f276820e
Stegur <stegura.alex@gmail.com>
2020-02-01 15:21:49 +0300

    winLose

---
+++ src/winLose.js
@@ -0,0 +1,33 @@
+const winLose = (a, b) => {
+    if (a === 0 && b === 0) {
+        return [-1, -1];
+    }
+
+    if (a === 1 && b === 1) {
+        return [1, 1];
+    }
+
+    if (a === 2 && b === 2) {
+        return [3, 3];
+    }
+
+    if (a === 0 && b === 2) {
+        return [3, -1];
+    }
+
+    if (a === 2 && b === 0) {
+        return [-1, 3];
+    }
+
+    if (a === 0 && b === 1) {
+        return [1, 0];
+    }
+
+    if (a === 1 && b === 0) {
+        return [0, 1];
+    }
+
+    return false;
+}
+
+module.exports = winLose
