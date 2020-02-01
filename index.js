2ebe3ecb898a646ae6724aa1523e5b29af0b2d72
Руслан Муфтиев <muftik@yandex-team.ru>
2020-02-01 19:47:02 +0300

    feat(server): Обработчик запросов

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 const { json } = require('micro');
+const { State, stateStorage } = require('./src/state');
+const { winLose } = require('./src/winLose');
+const { randomAnswer } = require('.src/randomAnswer');
+
 module.exports = async (req, res) => {
   const { request, session, version } = await json(req);
-  let resText = request.original_utterance || 'Над столицей империи наступила ночь. Пламя факела освещает полупустой трактирный зал. За угловым столиком сидят четверо. Они тихо переговариваются между собой, стараясь не привлекать к себе лишнего внимания.';
-  console.log('tokens', request.nlu);
-  if (request.nlu && request.nlu.tokens && ( request.nlu.tokens.indexOf('помощь') >= 0 || ( request.nlu.tokens.indexOf('умеешь') >= 0 && request.nlu.tokens.indexOf('что') >= 0 ))) {
-    resText = 'Я мастер игры в подземелья и драконы. Давайте продолжим игру.';
+  const db = require('./src/db');
+  const userId = session.user_id;
+  const sessionId = session.session_id;
+  const isStart = stateStorage.checkSession(userId, sessionId);
+
+  const currentState = isStart ? new State() : stateStorage.getState(userId);
+
+  const currentLoop = currentState.getloopIndex();
+
+  let resText = '';
+  let resTts = '';
+  if (currentLoop > 0) {
+    const botInfo = currentState.getBotInfo();
+    const userInfo = currentState.getUserInfo();
+    const botAnswers = db.getBotAnswers(currentLoop);
+    const botAnswer = randomAnswer(botInfo.strategy, botAnswers);
+
+    resText += botAnswer;
+    resTts += `<speaker effect="pitch_down">${botAnswer}</speaker>`;
+
+    const points = winLose(userInfo.strategy, botAnswer.strategy);
+    currentState.addUserPoints(points[0]);
+    currentState.addBotPoints(points[1]);
+    currentState.setBotStrategy(botAnswer.strategy);
+    currentState.loopNext();
+  } else {
+    resText = 'Над столицей империи наступила ночь. Пламя факела освещает полупустой трактирный зал. За угловым столиком силите вы и ваш напарник. Вы тихо переговариваетесь между собой, стараясь не привлекать к себе лишнего внимания. Как нам справедливо поделить добычу? ';
+    resTts = 'Над столицей империи наступила ночь. Пламя факела освещает полупустой трактирный зал. За угловым столиком силите вы и ваш напарник. Вы тихо переговариваетесь между собой, стараясь не привлекать к себе лишнего внимания. <speaker effect="pitch_down">Как нам справедливо поделить добычу?</speaker>';
+    const question = db.getQuestion(currentLoop);
+    resText += question;
+    resTts += `<speaker effect="pitch_down">${question}</speaker>`;
   }
+
   res.end(JSON.stringify({
     version,
     session,
-    response: { text: resText, end_session: false }
+    response: { text: resText, tts: resTts, end_session: false }
   }));
 };
