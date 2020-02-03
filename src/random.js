6ed7b5014fe5f4327445c71ac27814ce8a5a6108
sergw <Sergey.Voronezhskiy@gmail.com>
2020-02-03 04:07:23 +0300

    fix(prod): change logic & text generation & score calculation

--- src/random.js
+++ src/random.js
@@ -1,10 +1,12 @@
-const { filterAnswer } = require('../src/filter');
 const { _ } = require('underscore');
+const { filterAnswer } = require('../src/filter');
 
 module.exports = {
-    randomAnswer: function(ai_strategy, player_strategy, ai_answers) {
-        let answers = filterAnswer(ai_strategy, ai_answers);
-        let randomNumber = _.random(0, answers.length - 1);
-        return answers[randomNumber];
-    }
-}
+  randomAnswer(botStrategy, userStrategy, botAnswersAll) {
+    // TODO: поидее бот не должен знать о стратегии пользователя, но тут можно усложнить игру
+    // или заранее продумать стратегию и придерживаться ей, вместо тупого рандома
+    const answers = filterAnswer(botStrategy, botAnswersAll);
+    const randomNumber = _.random(0, answers.length - 1);
+    return answers[randomNumber];
+  },
+};
--- src/state.js
+++ src/state.js
@@ -1,97 +1,98 @@
-'use strict';
-
 const _ = require('underscore');
-const assert = require('assert');
 
 class State {
+  constructor(startState = {}) {
+    const {
+      loopIndex, userStrategy, userPoints, botStartegy, botPoints,
+    } = startState;
 
-    constructor(startState = {}) {
-        const { loopIndex, userStrategy, userPoints, botStartegy, botPoints } = startState;
+    this._loopIndex = loopIndex || 0;
+    this._userStrategy = userStrategy || _.random(0, 2);
+    this._userPoints = userPoints || 0;
+    this._botStartegy = botStartegy || _.random(0, 2);
+    this._botPoints = botPoints || 0;
+  }
 
-        this._loopIndex = loopIndex || 0;
-        this._userStrategy = userStrategy || _.random(0, 2);
-        this._userPoints = userPoints || 0;
-        this._botStartegy = botStartegy || _.random(0, 2);
-        this._botPoints = botPoints || 0;
+  getloopIndex() {
+    return this._loopIndex;
+  }
+
+  getUserInfo() {
+    return {
+      strategy: this._userStrategy,
+      points: this._userPoints,
+    };
+  }
+
+  getBotInfo() {
+    return {
+      strategy: this._botStartegy,
+      points: this._botPoints,
+    };
+  }
+
+  loopNext() {
+    this._loopIndex += 1;
+    return this._loopIndex;
+  }
+
+  gameOver(loopsLength) {
+    return loopsLength - 1 >= this._loopIndex;
+  }
+
+  setUserStrategy(strategy) {
+    this._userStrategy = parseInt(strategy, 10);
+  }
+
+  addUserPoints(points) {
+    this._userPoints += parseInt(points, 10);
+  }
+
+  setBotStrategy(strategy) {
+    this._botStartegy = parseInt(strategy, 10);
+  }
+
+  addBotPoints(points) {
+    this._botPoints += parseInt(points, 10);
+  }
+
+  // TODO: убрать из стейта
+  calcUserScore() {
+    const sum = Math.max(this._botPoints, 0) + Math.max(this._userPoints, 0);
+    if (sum === 0) {
+      return 0;
     }
-
-    getloopIndex() {
-        return this._loopIndex;
-    }
-
-    getUserInfo() {
-        return {
-            strategy: this._userStrategy,
-            points: this._userPoints
-        }
-    }
-
-    getBotInfo() {
-        return {
-            strategy: this._botStartegy,
-            points: this._botPoints
-        }
-    }
-
-    loopNext() {
-        this._loopIndex++;
-        return this._loopIndex;
-    }
-
-    gameOver(loopsLength) {
-        return loopsLength - 1 >= this._loopIndex;
-    }
-
-    setUserStrategy(strategy) {
-        this._userStrategy = parseInt(strategy);
-    }
-
-    addUserPoints(points) {
-        this._userPoints += parseInt(points);
-    }
-
-    setBotStrategy(strategy) {
-        this._botStartegy = parseInt(strategy);
-    }
-
-    addBotPoints(points) {
-        this._botPoints += parseInt(points);
-    }
-
-    //TODO: убрать из стейта
-    calcUserScore() {
-        const sum = this._botPoints + this._userPoints;
-        return parseInt( this._userPoints / sum * 100 )
-    }
+    return parseInt((this._userPoints / sum) * 100, 10);
+  }
 }
 
 class StateStorage {
-    constructor() {
-        this.states = {};
-    }
+  constructor() {
+    this.states = {};
+  }
 
-    getState(userId) {
-        return this.states[userId].state;
-    }
+  getState(userId) {
+    return this.states[userId].state;
+  }
 
-    checkSession(userId, sessionId) {
-        if (!this.states[userId]) {
-            return false;
-        }
-        return this.states[userId].sessionId === sessionId;
+  checkSession(userId, sessionId) {
+    if (!this.states[userId]) {
+      return false;
     }
+    return this.states[userId].sessionId === sessionId;
+  }
 
-    setState(userId, sessionId, state) {
-        this.states[userId] = {
-            sessionId,
-            state: new State(state)
-        }
-    }
+  setState(userId, sessionId, state) {
+    this.states[userId] = {
+      sessionId,
+      state: new State(state),
+    };
+  }
 }
 
 const stateStorage = new StateStorage();
 
 module.exports = {
-    State,
-    stateStorage
+  State,
+  stateStorage,
 };
