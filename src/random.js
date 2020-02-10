a217a3c98a280e6380257e2fa334b385f2fb06e2
Руслан Муфтиев <muftik@yandex-team.ru>
2020-02-10 19:05:16 +0300

    fix(core) Use 2 strategies instead of 3

--- src/random.js
+++ src/random.js
@@ -1,12 +1,10 @@
 const { _ } = require('underscore');
-const { filterAnswer } = require('../src/filter');
 
 module.exports = {
-  randomAnswer(botStrategy, userStrategy, botAnswersAll) {
+  randomAnswer(botStrategy, userStrategy, botAnswers) {
     // TODO: поидее бот не должен знать о стратегии пользователя, но тут можно усложнить игру
     // или заранее продумать стратегию и придерживаться ей, вместо тупого рандома
-    const answers = filterAnswer(botStrategy, botAnswersAll);
-    const randomNumber = _.random(0, answers.length - 1);
-    return answers[randomNumber];
+    const randomNumber = _.random(0, botAnswers.length - 1);
+    return botAnswers[randomNumber];
   },
 };
